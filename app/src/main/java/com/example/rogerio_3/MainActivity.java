31e22fc82bb3a1b990e41914dbbef9a00816b597
Luiz Teixeira <luiz_ecia@hotmail.com>
2019-04-11 03:28:17 -0300

    2 commit

--- app/src/main/java/com/example/rogerio_3/MainActivity.java
+++ app/src/main/java/com/example/rogerio_3/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final android.R.attr R = ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
